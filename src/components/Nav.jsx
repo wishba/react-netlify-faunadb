e0025475dc64edd4ef8c98020e1e9dea912b63c7
wishba <wisnubayu34@gmail.com>
2023-07-18 11:51:37 +0700

    login or logout condition

---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import netlifyIdentity from 'netlify-identity-widget';
+
+function Nav() {
+  netlifyIdentity.init()
+
+  const [button, setButton] = useState('Login')
+  useEffect(() => {
+    if (netlifyIdentity.currentUser() !== null) {
+      setButton('Logout')
+    }
+  }, [])
+
+  netlifyIdentity.on('login', () => location.reload())
+  netlifyIdentity.on('logout', () => location.reload())
+
+  return (
+    <nav>
+      <ul><li><strong>Todo app</strong></li></ul>
+      <ul><li><a href='#' role='button' onClick={() => netlifyIdentity.open()}>{button}</a></li></ul>
+    </nav>
+  )
+}
+
+export default Nav
