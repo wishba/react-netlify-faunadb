404795d8b9d64250739777e24d9d3ed9435d6bd5
wishba <wisnubayu34@gmail.com>
2023-07-18 09:18:37 +0700

    test create todo

---
+++ netlify/functions/createTodo.js
@@ -0,0 +1,39 @@
+const { default: fetch } = require("node-fetch")
+
+exports.handler = async (event, context) => {
+  const { userId, title, completed } = JSON.parse(event.body);
+
+  const query = `
+    mutation CreateTodo {
+      createTodo(
+        data: {
+          userId: "${userId}"
+          title: "${title}"
+          completed: ${completed}
+        }
+      ) {
+        userId
+        title
+        completed
+      }
+    }
+  `
+
+  const response = await fetch('https://graphql.fauna.com/graphql', {
+    method: 'POST',
+    headers: { Authorization: `Bearer ${process.env.FAUNA_SECRET_KEY}` },
+    body: JSON.stringify({ query }),
+  })
+
+  if (!response.ok) {
+    return {
+      statusCode: response.status,
+      body: 'Failed to create a new todo item.',
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(await response.json()),
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,55 @@ import netlifyIdentity from 'netlify-identity-widget';
 
 function App() {
   netlifyIdentity.init()
-  console.log(netlifyIdentity.currentUser().id);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const userId = netlifyIdentity.currentUser().id;
+    const title = 'Build an awesome app!';
+    const completed = false;
+
+    const response = await fetch('/.netlify/functions/createTodo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userId, title, completed }),
+    })
+
+    if (response.ok) {
+      console.log('Create a new todo is successful');
+    } else {
+      console.error('Failed to create a new todo item.');
+    }
+  }
 
   return (
     <>
-      <nav className='container-fluid'>
-        <ul><li><strong>Todo app</strong></li></ul>
-        <ul>
-          <li>
-            <a href='#' role='button' onClick={() => netlifyIdentity.open()}>Login</a>
-          </li>
-        </ul>
-      </nav>
+      <header className='container-fluid'>
+        <nav>
+          <ul><li><strong>Todo app</strong></li></ul>
+
+          <ul>
+            <li><a href='#' role='button' onClick={() => netlifyIdentity.open()}>Login</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className='container'>
+        <button onClick={handleSubmit}>tes</button>
+
+        <form action="">
+          <label htmlFor="">todo</label>
+          <input type="text" name="" id="" />
+          <label htmlFor="">
+            completed &nbsp;
+            <input type="checkbox" name="" id="" />
+          </label>
+          <br />
+          <input type="submit" value="submit" />
+        </form>
+      </main>
     </>
   )
 }
